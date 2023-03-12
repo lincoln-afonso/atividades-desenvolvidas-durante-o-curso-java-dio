5979c815d2482124822a66145573c7bf7d0189a2
lincoln-afonso <lincolnafonso223@gmail.com>
2023-03-12 09:41:44 -0300

    atualizando lista de atividades sobre estrutura de dados

--- atividades-de-estrutura-de-dados/atividade2/src/app/App.java
+++ atividades-de-estrutura-de-dados/atividade2/src/app/App.java
@@ -1,12 +1,68 @@
 package app;
 
+import java.util.Scanner;
+
+/* O Problema Josephus é um jogo de suicídio em massa que é realizado do seguinte
+ * modo: n pessoas, numeradas de 1 a n, estão sentadas em uma mesa circular. Iniciando
+ * da pessoa 1, um revolver contendo uma bala é passado entre elas. Após m passos, a
+ * pessoa que está com a arma com a bala na agulha comete suicídio, o corpo é removido,
+ * o ciclo de pessoas se fecha, e o jogo continua com a pessoa que está sentada após o
+ * elemento que cometeu o suicídio. O último sobrevivente é aquele que permanecerá após
+ * a morte dos demais. 
+ */
 public class App {
+    private Scanner leia;
+
+    public App() {
+        this.leia = new Scanner(System.in);
+    }
+
+    public Scanner getLeia() {
+        return this.leia;
+    }
+
+    public void cadastrarJogador(int numeroPessoas, Lista lista) {
+        String nome;
+
+        if (numeroPessoas < 2)
+            throw new IllegalArgumentException("Erro: Espera-se que tenha ao menos 2 jogadores participantes");
+
+        this.getLeia().nextLine();
+        for (int i = 0; i < numeroPessoas; i++) {
+            do {
+                System.out.print((i + 1) + "º jogador: ");
+                nome = this.getLeia().nextLine();
+            } while (nome.isEmpty());
+            lista.add(nome);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Lista lista = new Lista();
+        Scanner leia = new Scanner(System.in);
+        App app = new App();
+        String ganhador;
+        int numeroPessoas;
+        int numeroPassos;
 
-        lista.add("Lincoln");
-        lista.add("Afonso");
-        lista.add("Maria");
+        System.out.println("\nProblema Josephus");
+        try {
+            System.out.print("Informe o número de jogadores participantes: ");
+            numeroPessoas = app.getLeia().nextInt();
+            app.cadastrarJogador(numeroPessoas, lista);
 
+            System.out.print("\nInforme o número de passos: ");
+            numeroPassos = app.getLeia().nextInt();
+
+            System.out.println("\nJogadores");
+            for (int i = 0; i < lista.size(); i++) {
+                System.out.print("Jogador " + (i + 1) + ": " + lista.get(i));
+            }
+            ganhador = lista.remove(numeroPassos);
+            System.out.println("\nJogador(a) vencedor(a): " + ganhador);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        leia.close();
     }
 }
--- atividades-de-estrutura-de-dados/atividade2/src/app/Lista.java
+++ atividades-de-estrutura-de-dados/atividade2/src/app/Lista.java
@@ -41,6 +41,23 @@ public class Lista {
         return this.getTamanho();
     }
 
+    public String get(int indice) {
+        No noAuxiliar = this.getPrimeiroNo();
+
+        if (this.isEmpty())
+            throw new IndexOutOfBoundsException("Erro: A lista apresenta " + this.getTamanho() + "elemento(s)!\n");
+
+        if (indice == 0)
+            return this.getPrimeiroNo().getNome();
+        else if (indice == this.getTamanho() - 1)
+            return this.getUltimoNo().getNome();
+
+        for (int i = 0; i < indice; i++)
+            noAuxiliar = noAuxiliar.getProximoNo();
+
+        return noAuxiliar.getNome();
+    }
+
     public void add(String nome) {
         No novoNo = new No(nome);
 
@@ -52,8 +69,55 @@ public class Lista {
             novoNo.setNoAnterior(this.getUltimoNo());
             this.setUltimoNo(novoNo);
             novoNo.setProximoNo(this.getPrimeiroNo());
+            this.getPrimeiroNo().setNoAnterior(this.getUltimoNo());
         }
         this.tamanho++;
+    }
+
+    /**
+     * Remove um jogador a cada um número determinado de passos.
+     * 
+     * @param numerosPassos
+     * @exception IllegalArgumentException
+     * @return retorna o nó/referência correspondente do jogador vencedor (uníco
+     *         sobrevivente).
+     */
+    public String remove(int numerosPassos) {
+        No noAuxiliar = this.getPrimeiroNo();
+        No noAjudante = noAuxiliar;
+        int contPassos;
+
+        if (numerosPassos < 2)
+            throw new IllegalArgumentException("Erro: Espera-se que o número de passos seja maior ou igual a dois!\n");
+
+        while (this.getTamanho() > 1) {
+            noAuxiliar = noAjudante;
+            contPassos = 0;
+            while (contPassos < numerosPassos) {
+                noAuxiliar = noAuxiliar.getProximoNo();
+                noAjudante = noAuxiliar.getProximoNo();
+                contPassos++;
+            }
+            if (noAuxiliar == this.getUltimoNo()) {
+                noAuxiliar.getNoAnterior().setProximoNo(this.getPrimeiroNo());
+                this.getPrimeiroNo().setNoAnterior(noAuxiliar.getNoAnterior());
+                this.setUltimoNo(noAuxiliar.getNoAnterior());
+
+            } else if (noAuxiliar == this.getPrimeiroNo()) {
+                this.getUltimoNo().setProximoNo(this.getPrimeiroNo().getProximoNo());
+                this.getPrimeiroNo().getProximoNo().setNoAnterior(this.getUltimoNo());
+                this.setPrimeiroNo(noAuxiliar.getProximoNo());
+            } else {
+                noAuxiliar.getNoAnterior().setProximoNo(noAuxiliar.getProximoNo());
+                noAuxiliar.getProximoNo().setNoAnterior(noAuxiliar.getNoAnterior());
+            }
+            noAuxiliar.setNoAnterior(null);
+            noAuxiliar.setProximoNo(null);
+            this.tamanho--;
+            System.out.println(noAuxiliar.getNome());
+        }
+
+        return this.getUltimoNo().getNome();
     }
 
     public String toString() {
